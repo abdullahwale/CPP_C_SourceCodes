49a032ed78b31a31fb22470490c49bd16adea68b
abdullahwale <abdullahwale@gmail.com>
2021-01-12 19:24:56 +0500

    two programs

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	//Simple Variables  handle ought possible
+	int a,b,c;
+	a=20;
+	b=20;
+	c=30;
+	//Array---loop handle
+	int arr[1];
+	arr[0]=20;
+	arr[3]=30;
+	cout<<"Third Element Value"<<arr[99];
+	//Consecitive memory locations same type
+	cout<<"Array";
+}
---
+++ nestedIfElse.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b,c;
+	cout<<"Enter Three Values";
+	cin>>a>>b>>c;
+	if(a>b)//false
+	{
+		if(a>c)
+		cout<<"A is Maximaum";
+		else
+		cout<<"C is Maximum";
+	}
+	else
+	{
+		if(b>c)
+		cout<<"B is Maximum";
+		else
+		cout<<"C is Maximum"
+	}
+	
+	
+	
+	
+	
+	
+//	int a,b,c;
+//	cout<<"Enter three integer numbers";
+//	cin>>a>>b>>c;
+//	if(a==b)//True
+//	{
+//		if(b==c)//true
+//		cout<<"All Equal";
+//		else
+//		cout<<"Not Equal";
+//	}
+//	else
+//	cout<<"Not Equal";
+	
+		
+	
+}
