91717a78b62e6ab04c20bd8bac8c2f38b72b310b
abdullahwale <abdullahwale@gmail.com>
2021-01-13 19:12:08 +0500

    Create logicalOperatorss.cpp

---
+++ logicalOperatorss.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	cout<<"Compund Condition";
+	
+	char ch;
+	cout<<"Enter a character";
+	cin>>ch;
+	if(ch=='A'||ch=='a')
+	cout<<"Vowel";
+	else
+	cout<<"Consonent";
+	//And, Or 
+	//int a,b,c;
+	//cout<<"Enter three numbers";
+	//cin>>a>>b>>c;
+//	if(a>b&&a>c)
+//	cout<<a<<" is maximum";
+//	else if(b>a&&b>c)
+//	cout<<b<<" is Maximum";
+//	else
+//	cout<<c<<" is Maximum";
+}
