3c8b9321c73a090174a86364e060d8e0d7cddf72
abdullahwale <abdullahwale@gmail.com>
2021-02-01 19:56:30 +0500

    nested

---
+++ for loop practice.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+
+float c;
+for(int f=50;f<=100;f=f+5)
+{
+	c=5.0/9.0*(f-32);
+	printf("%d\t%6.2f\n",f,c);
+}
+
+
+//a to z
+//printf("Capital\tSmall\n");
+//for(char ch='a',cha='A';ch<='z';ch++,cha++)
+//{
+////	if(ch=='l'||ch=='v')
+////	printf("\n");
+//	printf("  %c\t%c\n",cha,ch);
+//}
+//	printf("\nFor Loop Practice");
+}
---
+++ nested.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+main()
+{
+//Nested for
+for(int i=1;i<=5;i++)
+{
+	for(int j=1;j<=3;j++)
+	{
+		cout<<"Outer:"<<i<<"\t";
+		cout<<"Inner:"<<j<<endl;
+	}
+	cout<<"\nNext Move\n";
+}
+
+
+
+
+	
+	//nested
+//	while()
+//	{
+//		while()
+//		{
+//			
+//		}
+//	 } 
+//	
+	
+	
+	
+	
+//	if()
+//	{
+//		if()
+//		{
+//		   if()
+//		   {
+//		   	 for()
+//		   	 {
+//		   	 	 while()
+//		   	 	 {
+//		   	 	 	if()
+//		   	 	 	{
+//		   	 	 		
+//						 }
+//		   	 	 	
+//					 }
+//				}
+//		   }
+//		}
+//	}
+}
