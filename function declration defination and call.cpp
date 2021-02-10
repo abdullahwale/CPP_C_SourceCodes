95707e7cff43f8596c52e9eb5efc2ee6bada797f
abdullahwale <abdullahwale@gmail.com>
2021-02-10 19:11:55 +0500

    Create function declration defination and call.cpp

---
+++ function declration defination and call.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void second();
+void Secratery()
+{
+	printf("Slam\n");
+	second();
+}
+
+main()
+{
+
+Secratery();
+
+}
+void second()
+{
+	for(int i=0;i<=5;i++)
+	{
+		printf("Abdullah Sab\n");
+	}
+	
+}
+
