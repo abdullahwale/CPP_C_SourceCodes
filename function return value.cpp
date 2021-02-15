98adddb2733e236a04a5ba723a16f6c18e3f58dc
abdullahwale <abdullahwale@gmail.com>
2021-02-15 15:06:06 +0500

    Create function return value.cpp

---
+++ function return value.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//int n=9;
+int simple(int n)
+{
+
+return  (n*n*n);	
+} 
+main()
+{
+	int n;
+printf("Enter a value");
+scanf("%d",&n);
+	int re=simple(n);//
+	printf("%d",re);
+	
+}
